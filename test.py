ba44383fa51008b1663153259725955e862f409a
ls9kf <ls9kf@mst.edu>
2019-03-01 23:57:31 -0600

    Got connection workinggit add -f *

--- test.py
+++ test.py
@@ -4,7 +4,14 @@
 
 import pymongo
 
-client = pymongo.MongoClient("mongodb://root:trackstar2019@track-tyc0p.gcp.mongodb.net/test?retryWrites=true")
+client = pymongo.MongoClient("mongodb+srv://root:trackstar@track-tyc0p.gcp.mongodb.net/test?retryWrites=true")
 db = client.track
+baseball = db.Baseball
+marathon = db.Marathon
+triathlon = db.Triathlon
 
+agility = { "A" : [["Barbell back squat", "5x5",  10], ["Bodyweight vertical jumps", "5x5", 120]],
+        "B" : [["Barbell good mornings","5x5", 10], ["Broad jumps", "5x5", 120]]}
+
+baseball.insert_one(agility)
 
