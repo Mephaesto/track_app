1c49b729c3f79891f047dcc9b3efde2b37203a8b
ls9kf <ls9kf@mst.edu>
2019-03-02 21:02:39 -0600

    woot

--- test.py
+++ test.py
@@ -16,13 +16,21 @@ baseball = db.Baseball
 marathon = db.Marathon
 triathlon = db.Triathlon
 df = pd.read_csv("Baseball - Sheet1.csv")
-#maradf = pd.read_csv("Marathon 16 weeks - Sheet1.csv")
-#triadf = pd.read_csv("Triathlon - Sheet1.csv")
+maradf = pd.read_csv("Marathon 16 weeks - Sheet1.csv")
+triadf = pd.read_csv("Triathlon - Sheet1.csv")
 
 records_ = df.to_dict('records')
-#mara_records_ = maradf.to_dict(orient = 'records')
-#tria_records_ = triadf.to_dict(orient = 'records')
+mara_records_ = maradf.to_dict(orient = 'records')
+tria_records_ = triadf.to_dict(orient = 'records')
 
 result = baseball.insert_many(records_)
+result1 = marathon.insert_many(mara_records_)
+result2 = triathlon.insert_many(tria_records_)
 
-print(result)
+#print(result)
+#print(result1)
+#print(result2)
+
+data = pd.DataFrame(list(baseball.find({'Week': 3})))
+
+print(data)
